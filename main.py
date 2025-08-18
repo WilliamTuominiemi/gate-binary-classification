aad596786100e5ea3e556d12e3b80cc314abed61
williamtuominiemi <william.m.tuominiemi@gmail.com>
2025-08-18 22:15:32 +0300

    fix functions

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import math
 import random
 
 def sigmoid(z):
-    return 1 / ( 1 + (math.e)^(-z) )
+    return 1 / ( 1 + math.exp(-z) )
 
 def forward_pass(w1, x1, w2, x2, b):
     z = w1 * x1 + w2 * x2 + b
@@ -10,7 +10,9 @@ def forward_pass(w1, x1, w2, x2, b):
     return a
 
 def compute_loss(y_true, y_pred):
-    loss = -(y_true * math.log(y_pred) + (1 - y_true) * math.log(1 - y_pred))
+    epsilon = 10**(-12)
+    a = min(max(y_pred, epsilon), 1 - epsilon) 
+    loss = -(y_true * math.log(a) + (1 - y_true) * math.log(1 - a))
     return loss
 
 def update_weights(w1, w2, b, x1, x2, y_true, y_pred, learning_rate):
@@ -26,13 +28,29 @@ def update_weights(w1, w2, b, x1, x2, y_true, y_pred, learning_rate):
     return w1, w2, b
 
 def train(X, Y, epochs, learning_rate):
-    w1 = random.randint(0, 10)
-    w2 = random.randint(0, 10)
-    b = random.randint(0, 10)
+    w1 = random.uniform(-0.5, 0.5)
+    w2 = random.uniform(-0.5, 0.5)
+    b = random.uniform(-0.5, 0.5)
 
     for epoch in range(epochs):
-        for x1, x2, y_true in zip(X, Y):
+        print("epoch " , epoch)
+        loss_y_true = []
+        loss_y_pred = []
+
+        for (x1, x2), y_true in zip(X, Y):
             y_pred = forward_pass(w1, x1, w2, x2, b)
             w1, w2, b = update_weights(w1, w2, b, x1, x2, y_true, y_pred, learning_rate)
+            loss_y_true.append(y_true)
+            loss_y_pred.append(y_pred)
+
+        print("loss: ", 
+              compute_loss(sum(loss_y_true) / len(loss_y_true), 
+                           sum(loss_y_pred) / len(loss_y_pred)))
     return w1, w2, b
 
+X = [(0, 0), (0, 1), (1, 0), (1, 1)]
+
+Y = [0, 0, 0, 1] # AND
+# Y = [0, 1, 1, 1] # OR
+
+train(X, Y, 10, 0.1)
